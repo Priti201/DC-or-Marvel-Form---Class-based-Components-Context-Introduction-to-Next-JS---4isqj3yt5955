d32544b941a908ee1748a106a80348c7e4f01626
Priti Gupta <132835431+Priti201@users.noreply.github.com>
2023-11-07 20:25:02 +0530

    Update FormA.js

--- app/FormA.js
+++ app/FormA.js
@@ -1,31 +1,39 @@
-'use client'
+"use client";
 import { useState } from "react";
 
 function FormA({ onSubmit, age }) {
-    const handleSubmit = (event) => {
-    };
-    return (
-        <form id="dc" onSubmit={handleSubmit}>
-            <h2>Form A</h2>
-            <label>
-                Select DC Shows:
-                <select  >
-                    <option value="">--Select--</option>
-                    <option value="The Flash">The Flash</option>
-                    <option value="Arrow">Arrow</option>
-                    <option value="Supergirl">Supergirl</option>
-                    <option value="Legends of Tomorrow">Legends of Tomorrow</option>
-                </select>
-            </label>
-            <br />
-            <label>
-                Enter your age:
-                <input type="number" value={age} disabled />
-            </label>
-            <br />
-            <button id="submit-dc" type="submit">Submit</button>
+  const [show, setShow] = useState("");
 
-        </form>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(show){
+      onSubmit(show);
+    }
+  };
+  console.log({ show });
+  return (
+    <form id="dc" onSubmit={handleSubmit}>
+      <h2>Form A</h2>
+      <label>
+        Select DC Shows:
+        <select onChange={(e) => setShow(e.target.value)}>
+          <option value="">--Select--</option>
+          <option value="The Flash">The Flash</option>
+          <option value="Arrow">Arrow</option>
+          <option value="Supergirl">Supergirl</option>
+          <option value="Legends of Tomorrow">Legends of Tomorrow</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Enter your age:
+        <input type="number" value={age} disabled />
+      </label>
+      <br />
+      <button id="submit-dc" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 export default FormA;
